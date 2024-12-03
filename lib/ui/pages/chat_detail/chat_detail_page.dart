58fbbce41058749897bb6bb82172497c534b72d6
1125bid <dlfdlfdldh1125@naver.com>
2024-12-03 11:30:03 +0900

    feat: basic settings

---
+++ lib/ui/pages/chat_detail/chat_detail_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ChatDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Text('ChatDetailPage'));
+  }
+}
